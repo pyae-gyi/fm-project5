fe8d31b3e04b4cd88367867994dcfd46d2da5bb3
Pyae Sone Khant <pyaekhant@my.uopeople.edu>
2024-05-08 16:01:15 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
             </div>
           </div>
           {stateData.isError ? (
-            <div className="font-extrabold text-5xl italic leading-5">
+            <div className="font-extrabold text-5xl italic ">
               Please Try Again!
             </div>
           ) : stateData.loading ? (
